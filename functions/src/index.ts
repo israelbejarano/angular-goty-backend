aafe57df222cc3f9a455ec9da46976bef72881fb
israel <israel.bejarano.a@gmail.com>
2019-12-08 19:04:28 +0100

    add files to ignore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,12 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
+const serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://firechat-a22a3.firebaseio.com"
+});
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
